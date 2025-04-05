f669fbd4c25693550ac799d3a1575e31ccd3561f
t-uesawa <t.uesawa.works@gmail.com>
2025-04-05 19:23:30 +0900

    first push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPwa from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 他の設定をここに追加
+};
+
+export default nextPwa({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
